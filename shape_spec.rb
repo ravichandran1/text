364e94cc05b70ceaaefc94212766f7f64b6475be
ravichandran <ravichandran@railslauncher.com>
2015-09-22 11:11:02 +0530

    shape_spec.rb

--- shape_spec.rb
+++ shape_spec.rb
@@ -1,11 +1,23 @@
 require "rspec"
-require './calculator'
+require './shape'
 
- describe shape do
-    describe "area" do
-      it "rectangle" do
-           sha=shape.new
-           expect(sha.rectangle(width:100,height:100)).to eql(10000)
-      end
+describe Shape do
+  describe "area" do
+    it "rectangle" do
+     sha=Shape.new
+     expect(sha.rectangle(width:100,height:100)).to eql(10000)
     end
- end          
+  end
+  describe "area" do
+    it "triangle" do
+     sha=Shape.new
+     expect(sha.triangle(width:100,height:100)).to eql(10000)
+    end
+  end
+  describe "area" do
+    it "square" do
+     sha=Shape.new
+     expect(sha.square(width:100,height:100)).to eql(10000)
+    end
+  end    
+end          
