7d4801b0d05e697cc4c0c3657b6cb2538f0851d1
ravichandran <ravichandran@railslauncher.com>
2015-09-24 16:56:15 +0530

    add minimum/maximum values

--- arrayprocessor_spec.rb
+++ arrayprocessor_spec.rb
@@ -3,21 +3,33 @@ require './arrayprocessor'
 
 describe  ArrayProcessor do
   describe 'add_tatal' do
-    it "[20, 4, 2, 40]" do
+    it "gives add total values" do
       array=ArrayProcessor.new
       expect(array.add_total).to eql(66)
     end
   end
   describe 'acending_order' do
-    it "[20, 4, 2, 40]" do
+    it "gives acending order values" do
       array=ArrayProcessor.new
       expect(array.acending_order).to eql([2,4,20,40])
     end
   end
    describe 'descending_order' do
-    it "[20, 4, 2, 40]" do
+    it "gives descending order values" do
       array=ArrayProcessor.new
       expect(array.descending_order).to eql([40,20,4,2])
+    end
+  end
+   describe 'minimum_value' do
+    it "gives minimum value" do
+      array=ArrayProcessor.new
+      expect(array.minimum_value).to eql(2)
+    end
+  end
+  describe 'maximum_value' do
+    it "gives maximum value" do
+      array=ArrayProcessor.new
+      expect(array.maximum_value).to eql(40)
     end
   end
 end  
